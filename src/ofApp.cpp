aa78a6195d05f36d21bf985127efdba03f4275bc
笠原未来 <fum_o3o7@ezweb.ne.jp>
2018-12-30 23:55:31 +0900

    test

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,9 +12,16 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-ofDrawBitmapStringHighlight("Everything works!", 20, 20);
+    ofDrawBitmapStringHighlight("Everything works!", 20, 20);
     ofDrawLine(10,10,100,100);
     ofDrawCircle(200,200,100);
+    ofRectangle myRect;
+    myRect.x = 10;
+    myRect.y = 10;
+    myRect.width = 100;
+    myRect.height = 100;
+    
+    ofDrawRectRounded( myRect, 10, 20, 30, 40 );
 }
 
 //--------------------------------------------------------------
