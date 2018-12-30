e25476f1f67598dc14efbcfbe2c3b9d069c1cfba
笠原未来 <fum_o3o7@ezweb.ne.jp>
2018-12-31 01:35:54 +0900

    アニメーション基礎

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,23 +1,35 @@
 #include "ofApp.h"
 
+float x;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetBackgroundColor(78,25,187);  //背景 r,g,b
+    x = 0.0;
+    ofSetBackgroundAuto(false); // 軌跡を残す = アニメーションで線を描ける
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update(){ // 変数の更新 = アニメーション
+    
+    if (x < 800) {
+        x += 0.2;
+    }else if(x >= 800) {
+        x = 0.0;
+    }
+    
+
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw(){ // 図形の描写
     ofDrawBitmapStringHighlight("Everything works!", 20, 20);
     ofDrawLine(10,10,100,100);
     ofDrawCircle(200,200,100); //円 x,y,半径
     ofEnableAlphaBlending();
-    ofDrawRectRounded( 10, 10, 100, 100, 50 ); //角丸 x,y,width,height,radius
+    ofDrawRectRounded( 10 + x, 10, 100, 100, 50 ); //角丸 x,y,width,height,radius
     
     ofEnableAlphaBlending();    // turn on alpha blending
     ofSetColor(255,0,0,50);    // red, 50% transparent
